7a623c0998ff70272446e48f25e59acdd49a8a7b
Wanxin Lin <linwx_ustc@163.com>
2019-01-27 10:38:34 +0800

    删了一些页面和Controller

--- src/main/java/com/my/blog/website/controller/admin/CommentController.java
+++ src/main/java/com/my/blog/website/controller/admin/CommentController.java
@@ -6,7 +6,6 @@ import com.my.blog.website.model.Bo.RestResponseBo;
 import com.my.blog.website.model.Vo.CommentVo;
 import com.my.blog.website.model.Vo.CommentVoExample;
 import com.my.blog.website.model.Vo.UserVo;
-import com.my.blog.website.service.ICommentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -24,61 +23,14 @@ public class CommentController extends BaseController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CommentController.class);
 
-    @Resource
-    private ICommentService commentsService;
+
 
     @GetMapping(value = "")
     public String index(@RequestParam(value = "page", defaultValue = "1") int page,
                         @RequestParam(value = "limit", defaultValue = "15") int limit, HttpServletRequest request) {
-        UserVo users = this.user(request);
-        CommentVoExample commentVoExample = new CommentVoExample();
-        commentVoExample.setOrderByClause("coid desc");
-        commentVoExample.createCriteria().andAuthorIdNotEqualTo(users.getUid());
-        PageInfo<CommentVo> commentsPaginator = commentsService.getCommentsWithPage(commentVoExample, page, limit);
-        request.setAttribute("comments", commentsPaginator);
+
         return "admin/comment_list";
     }
 
-    /**
-     * 删除一条评论
-     *
-     * @param coid
-     * @return
-     */
-    @PostMapping(value = "delete")
-    @ResponseBody
-    public RestResponseBo delete(@RequestParam Integer coid) {
-        try {
-            CommentVo comments = commentsService.getCommentById(coid);
-            if (null == comments) {
-                return RestResponseBo.fail("不存在该评论");
-            }
-            commentsService.delete(coid, comments.getCid());
-        } catch (Exception e) {
-            String msg = "评论删除失败";
-            LOGGER.error(msg, e);
-            return RestResponseBo.fail(msg);
-        }
-        return RestResponseBo.ok();
-    }
-
-    @PostMapping(value = "status")
-    @ResponseBody
-    public RestResponseBo delete(@RequestParam Integer coid, @RequestParam String status) {
-        try {
-            CommentVo comments = commentsService.getCommentById(coid);
-            if (comments != null) {
-                comments.setCoid(coid);
-                comments.setStatus(status);
-                commentsService.update(comments);
-            } else {
-                return RestResponseBo.fail("操作失败");
-            }
-        } catch (Exception e) {
-            String msg = "操作失败";
-            return RestResponseBo.fail(msg);
-        }
-        return RestResponseBo.ok();
-    }
 
 }
