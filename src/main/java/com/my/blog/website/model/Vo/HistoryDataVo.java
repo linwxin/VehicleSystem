c2186495d993ae765ab04c46d840aff89dbb2232
Wanxin Lin <linwx_ustc@163.com>
2019-02-18 17:49:07 +0800

    历史记录的DAO层

---
+++ src/main/java/com/my/blog/website/model/Vo/HistoryDataVo.java
@@ -0,0 +1,43 @@
+package com.my.blog.website.model.Vo;
+
+public class HistoryDataVo {
+    private String id;
+
+    private String vehicleType;
+
+    private String location;
+
+    private String date;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
